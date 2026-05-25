96849a3f765184c0b3392bc9f2793fcd687b7b64
freerobu67-dot <free.robu67@gmail.com>
2026-05-25 19:38:28 +0500

    Add boat freeze core mixin code

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,28 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(Player.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+
+    @Inject(method = "startRiding", at = @At("TAIL"))
+    private void freezeOnVehicleEnter(Entity vehicle, boolean force, CallbackInfoReturnable<Boolean> cir) {
+        Player player = (Player) (Object) this;
+
+        if (vehicle.getType().getDescriptionId().contains("boat")) {
+            // 1. Clears vertical Y momentum to stall you mid-air exactly like 1.16
+            Vec3 currentVelocity = player.getDeltaMovement();
+            player.setDeltaMovement(new Vec3(currentVelocity.x, 0.0, currentVelocity.z));
+            player.hurtMarked = true; 
+
+            // 2. Wipes the fall distance tracker
+            player.fallDistance = 0.0F;
+        }
+    }
 }
